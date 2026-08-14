208342e953f57a7b43b208c73108a4e1d692a64e
Daniel <dannyyung130@gmail.com>
2026-08-14 13:26:34 +0100

    Initial Yung Gaming Hub website

---
+++ Yung Gaming Hub/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dwgrmsxzejeumrykfyqv.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_zg_s4otHwaKK-EGQ6jJTxA_WdIjrsfY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
